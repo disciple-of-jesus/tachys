1949b4cc4a57560fd6011374bfdfd035922ea100
Disciple of Jesus <Disciple-of-jesus@github.com>
2025-08-22 08:41:18 +0200

    Start of writing a simple .wav file

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <fstream>
 #include <iostream>
 
 int main() {
@@ -33,6 +34,30 @@ int main() {
     for(int sampleIndex = 0; sampleIndex < numberOfSamples; sampleIndex++) {
         std::cout << audioSamples[sampleIndex] << std::endl;
     }
+
+    const std::string fileName = "Trianglewave.wav";
+
+    const int sampleRate = 44100;
+    const int channelSize = 1;
+    const int dataSize = 0;
+    
+    const int bitsPerByte = 8;
+    const int byteRate = sampleRate * channelSize * (bitDepth / bitsPerByte);
+
+    // How many bytes are in a single sample frame? 
+    // A sample frame means all channels (in mono: L, in stereo: L + R) at a single point in time.
+    const int blockAlign = channelSize * (bitDepth / bitsPerByte);
+
+    std::ofstream file(fileName, std::ios::binary);
+
+    // Every RIFF file starts with these 4 ASCII letters.
+    file.write("RIFF", 4);
+
+    // The next 4 bytes are the size of the entire file.
+    // That makes up 8 bytes in total. Then, there must be at least the word "WAVE" following it.
+    // Also, there must be at least a single (in this case empty) chunk.
+    // That makes up a total of 4 ("RIFF") + 4 (chunkSize) + 4 ("WAVE") + 24 ("fmt ") + 8 ("data") + 0 (dataSize) bytes.
+    const int chunkSize = 4 + 4 + 24 + 8 + dataSize;
 
     return 0;
 }
