c4d18a7984ddceb7274f7fc06ee521eafaa6ac8c
Disciple of Jesus <Disciple-of-jesus@github.com>
2025-08-22 09:30:15 +0200

    Writing the sample to a .WAV file

--- main.cpp
+++ main.cpp
@@ -32,13 +32,14 @@ int main() {
     }
 
     for(int sampleIndex = 0; sampleIndex < numberOfSamples; sampleIndex++) {
-        std::cout << audioSamples[sampleIndex] << std::endl;
+        // std::cout << audioSamples[sampleIndex] << std::endl;
     }
 
     const std::string fileName = "Trianglewave.wav";
 
     const int sampleRate = 44100;
     const int channelSize = 1;
+    const int pcmAudioFormat = 1;
     const int dataSize = 0;
     
     const int bitsPerByte = 8;
@@ -58,6 +59,45 @@ int main() {
     // Also, there must be at least a single (in this case empty) chunk.
     // That makes up a total of 4 ("RIFF") + 4 (chunkSize) + 4 ("WAVE") + 24 ("fmt ") + 8 ("data") + 0 (dataSize) bytes.
     const int chunkSize = 4 + 4 + 24 + 8 + dataSize;
+
+    // A variable lives in memory. &chunkSize gives the address of the variable chunkSize.
+    const int* chunkSizeMemoryAddress = &chunkSize;
+    const char* chunkSizeMemoryAddressAsChar = reinterpret_cast<const char*>(chunkSizeMemoryAddress);
+
+    std::cout << "The address of the chunkSize variable is " << chunkSizeMemoryAddress << "." << std::endl;
+
+    // Copy the raw (4) bytes of the chunkSize variable into the file so the RIFF header is complete.
+    file.write(chunkSizeMemoryAddressAsChar, 4);
+    file.write("WAVE", 4);
+
+    file.write("fmt ", 4);
+
+    // This chunk has a fixed value of 16 bytes.
+    const int fmtChunkSize = 16;
+
+    // Copy the raw (4) bytes of the fmtChunkSize variable into the file.
+    const int* fmtChunkSizeMemoryAddress = &fmtChunkSize;
+    const char* fmtChunkSizeMemoryAddressAsChar = reinterpret_cast<const char*>(fmtChunkSizeMemoryAddress);
+
+    file.write(fmtChunkSizeMemoryAddressAsChar, 4);
+    file.write(reinterpret_cast<const char*>(&pcmAudioFormat), 2);
+    file.write(reinterpret_cast<const char*>(&channelSize), 2);
+    file.write(reinterpret_cast<const char*>(&sampleRate), 4);
+    file.write(reinterpret_cast<const char*>(&byteRate), 4);
+    file.write(reinterpret_cast<const char*>(&blockAlign), 2);
+    file.write(reinterpret_cast<const char*>(&bitDepth), 2);
+
+    // Data subchunk
+    file.write("data", 4);
+
+    const int dataChunkSize = numberOfSamples * blockAlign;
+    file.write(reinterpret_cast<const char*>(&dataChunkSize), 4);
+
+    for(int sampleIndex = 0; sampleIndex < numberOfSamples; sampleIndex++) {
+        const char* sampleMemoryAddress = reinterpret_cast<const char*>(&audioSamples[sampleIndex]);
+
+        file.write(sampleMemoryAddress, bitDepth);
+    }
 
     return 0;
 }
